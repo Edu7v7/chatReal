453e80a82fbd3250c5d0ef64b13f524a2db02236
Alvaro Arostegui <aarostegui28@gmail.com>
2022-06-22 14:52:33 -0500

    Agregando algunos ajustes para las horas restantes

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,7 +4,7 @@ $(function() {
     //este es la conexion del socket del cliente    
     const socket = io();
 
-    ////obteniendo los elementos de DOM de nuestro formulario de la interface.
+    //obteniendo los elementos de DOM de nuestro formulario de la interface.
     const messageForm = $('#message-form');
     const messageBox = $('#message');
     const chat = $('#chat');
@@ -49,6 +49,7 @@ $(function() {
    socket.on('new message', function(data){
     // ahora vamos a recibir un objeto 
         chat.append('<b>'+ data.nick +'<b/>: ' +data.msg+ '<br/>');
+        // chat.append(`<p><span>${data.nick}:</span><span>${data.msg}</span></p>`);
    }); 
 
 
